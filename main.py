ae2ebf25939d84c0e3df4e27ac52f7ea6fcf7de0
valhallajazzy <valhallajazzy@mail.ru>
2023-10-15 14:53:35 +0300

    Refactor task

--- main.py
+++ main.py
@@ -1,56 +1,69 @@
+import os
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
+import argparse
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
+from dotenv import load_dotenv
+from collections import defaultdict
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-def company_years_old():
+def get_company_age():
     date_of_foundation = datetime.date(year=1920, month=1, day=1)
     delta = datetime.date.today() - date_of_foundation
-    years_old = int(delta.total_seconds()//86400//365)
-    if years_old % 100 > 4 and years_old % 100 < 21:
-        return f'{years_old} лет'
+    return int(delta.total_seconds()//86400//365)
+
+def choosing_the_word_by_age(age):
+    if age % 100 > 4 and age % 100 < 21:
+        return f'{age} лет'
+    remains = age % 100 % 10
+    if remains == 1:
+        return f'{age} год'
+    if remains > 1 and remains < 5:
+        return f'{age} года'
     else:
-        remains = years_old % 100 % 10
-        if remains == 1:
-            return f'{years_old} год'
-        if remains > 1 and remains < 5:
-            return f'{years_old} года'
-        else:
-            return f'{years_old} лет'
+        return f'{age} лет'
 
 
-def drink_category(drink_list):
-    drink_category_dict = {}
-    for elem in drink_list:
-        if elem['Категория'] not in drink_category_dict:
-            drink_category_dict[elem['Категория']] = []
+def create_file_path():
+    parser = argparse.ArgumentParser()
+    load_dotenv()
+    parser.add_argument('-p', '--path', default=f'{os.getenv("DRINKS_PATH")}')
+    return parser
 
+
+def sort_drinks(drink_list):
+    drink_category_dict = defaultdict(list)
     for elem in drink_list:
         drink_category_dict[elem['Категория']].append(elem)
     return drink_category_dict
 
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
+    template = env.get_template('template.html')
 
-excel_data_df2 = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', usecols=[
-    'Категория','Название','Сорт','Цена','Картинка','Акция'], na_values='nan', keep_default_na=False)
+    path_to_drinkdata = create_file_path().parse_args()
 
-list_data_drink = excel_data_df2.to_dict(orient='records')
+    excel_data_df2 = pandas.read_excel(f'{path_to_drinkdata.path}', sheet_name='Лист1', usecols=[
+        'Категория','Название','Сорт','Цена','Картинка','Акция'], na_values='nan', keep_default_na=False)
 
-rendered_page = template.render(
-    date_of_foundation=company_years_old(),
-    list_data_drink=drink_category(list_data_drink)
-)
+    list_data_drink = excel_data_df2.to_dict(orient='records')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    age = get_company_age()
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    rendered_page = template.render(
+        date_of_foundation=choosing_the_word_by_age(age),
+        list_data_drink=sort_drinks(list_data_drink)
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
